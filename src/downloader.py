711fe0d46d3e50c5bc56daf7f0c774b3d3325643
dawid <dawidkocyla25@wp.pl>
2026-08-24 05:23:57 +0200

    feat: initialize InstagramStoryDownloader class

---
+++ src/downloader.py
@@ -0,0 +1,5 @@
+class InstagramStoryDownloader:
+    #A class to automate downloading Instagram Stories using Playwritght
+    def __init__(self,session_id: str):
+        self.session_id = session_id
+
