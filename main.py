8db73a17ad02ec6a12675fc6a5d720bceb36876b
dawid <dawidkocyla25@wp.pl>
2026-08-26 21:28:34 +0200

    feat: add command line argument parsing and profile name validation

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from src.downloader import InstagramStoryDownloader
 from src import config
+import sys
 
 
 if __name__ == "__main__":
     downloader = InstagramStoryDownloader(config.SESSION_ID)
-    downloader.download_story("dodaqueen")
+    
+    if ( len(sys.argv) != 2):
+        print("Usage: python main.py <target_profile>")
+        sys.exit(1)
+
+    target_profile = sys.argv[1]
+    profile_lenght = len(target_profile)
+
+    if profile_lenght < 2 or profile_lenght > 30:
+        print("Error: targer profile name must be between 2 and 30 characters.")
+        sys.exit(1)
+
+    
+    print(f"Starting download for profile: {target_profile}") 
+    downloader.download_story(target_profile)
